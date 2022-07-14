186772ffa1d459bc081934e9423537aefc51aed1
Mark Goho <markgoho@gmail.com>
2022-07-14 17:47:38 +0000

    fix failing spec

--- apps/ite-portal/src/app/app.component.spec.ts
+++ apps/ite-portal/src/app/app.component.spec.ts
@@ -18,7 +18,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ite-portal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ite-portal2');
+    expect(app.title).toEqual('ite-portal');
   });
 
   it('should render title', () => {
