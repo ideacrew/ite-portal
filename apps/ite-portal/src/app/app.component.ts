b9397ff994e3374dad75e67377554efbb336cca7
Mark Goho <markgoho@gmail.com>
2022-07-18 16:12:51 +0000

    add button to get data

--- apps/ite-portal/src/app/app.component.ts
+++ apps/ite-portal/src/app/app.component.ts
@@ -1,4 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { mockData } from './data';
+
+export interface Transaction {
+  _id: {
+    $oid: string;
+  };
+  created_at: string;
+  extract_type: 'Admission' | 'Discharge' | 'Update';
+  failures: unknown;
+  payload: unknown;
+  provider_identifier: 'testing_db_connection';
+}
 
 @Component({
   selector: 'dbh-root',
@@ -7,4 +21,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ite-portal';
+
+  constructor(private http: HttpClient) {}
+
+  data$!: Observable<unknown>;
+
+  getData() {
+    this.data$ = this.http
+      .get('https://ite-api.herokuapp.com/api/v1/extracts')
+      .pipe(catchError(() => of(mockData)));
+  }
 }
