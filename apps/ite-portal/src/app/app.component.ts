e0ba0132b3323e353d157c09e56955dc64b33a07
Mark Goho <markgoho@gmail.com>
2022-07-24 21:36:44 +0000

    don't send form datta unless valid

--- apps/ite-portal/src/app/app.component.ts
+++ apps/ite-portal/src/app/app.component.ts
@@ -1,18 +1,26 @@
+/* eslint-disable unicorn/no-null */
 /* eslint-disable @typescript-eslint/unbound-method */
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { catchError, Observable, of } from 'rxjs';
 import { mockData } from './data';
 
-// interface ExtractTransmissionForm {
-//   provider_gateway_identifier: '73982';
-//   coverage_start: string;
-//   coverage_end: string;
-//   extracted_on: string;
-//   transaction_group: 'admission' | 'discharge' | null;
-//   file_type: 'Initial';
-// }
+interface ExtractTransmissionForm {
+  provider_gateway_identifier: FormControl<string | null>;
+  coverage_start: FormControl<string | null>;
+  coverage_end: FormControl<string | null>;
+  extracted_on: FormControl<string | null>;
+  transaction_group: FormControl<TransactionGroup | null>;
+  file_type: FormControl<string | null>;
+}
+
+type TransactionGroup = 'admission' | 'discharge';
 
 @Component({
   selector: 'dbh-root',
@@ -21,20 +29,26 @@ import { mockData } from './data';
 })
 export class AppComponent {
   title = 'ite-portal';
-
-  constructor(private http: HttpClient, private fb: FormBuilder) {}
+  debug = true;
 
   data$!: Observable<unknown>;
 
-  extractForm: FormGroup = this.fb.group({
-    provider_gateway_identifier: ['73982', Validators.required],
-    coverage_start: ['', Validators.required],
-    coverage_end: ['', Validators.required],
-    extracted_on: ['', Validators.required],
-    // eslint-disable-next-line unicorn/no-null
-    transaction_group: [null, Validators.required],
-    file_type: ['Initial', Validators.required],
-  });
+  extractForm!: FormGroup<ExtractTransmissionForm>;
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.extractForm = this.fb.group({
+      provider_gateway_identifier: this.fb.control('73982', [
+        Validators.required,
+      ]),
+      coverage_start: this.fb.control<string>('', [Validators.required]),
+      coverage_end: this.fb.control<string>('', [Validators.required]),
+      extracted_on: this.fb.control<string>('', [Validators.required]),
+      transaction_group: this.fb.control<TransactionGroup | null>(null, [
+        Validators.required,
+      ]),
+      file_type: this.fb.control('Initial', [Validators.required]),
+    });
+  }
 
   getData() {
     this.data$ = this.http
@@ -43,14 +57,15 @@ export class AppComponent {
   }
 
   sendData(): void {
-    this.http
-      .post(
-        // Url to post to
-        'https://ite-api.herokuapp.com/api/v1/extracts/ingest',
+    if (this.extractForm.status === 'VALID')
+      this.http
+        .post(
+          // Url to post to
+          'https://ite-api.herokuapp.com/api/v1/extracts/ingest',
 
-        // body of the payload, here sending the entire form value
-        this.extractForm.value
-      )
-      .subscribe();
+          // body of the payload, here sending the entire form value
+          this.extractForm.value
+        )
+        .subscribe();
   }
 }
