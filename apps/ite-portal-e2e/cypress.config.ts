ce661ec62afbac47ebd2e14a304df23f43b16f54
Mark Goho <markgoho@gmail.com>
2022-08-08 09:35:46 -0400

    turn off video recording in cypress

--- apps/ite-portal-e2e/cypress.config.ts
+++ apps/ite-portal-e2e/cypress.config.ts
@@ -4,13 +4,14 @@ import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 const cypressJsonConfig = {
   fileServerFolder: '.',
   fixturesFolder: './src/fixtures',
-  video: true,
+  video: false,
   videosFolder: '../../dist/cypress/apps/ite-portal-e2e/videos',
   screenshotsFolder: '../../dist/cypress/apps/ite-portal-e2e/screenshots',
   chromeWebSecurity: false,
   specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
   supportFile: 'src/support/e2e.ts',
 };
+
 export default defineConfig({
   e2e: {
     // eslint-disable-next-line unicorn/prefer-module
