36d3b41933b77c06126a5968a5563d8cfeb72445
Mark Goho <markgoho@gmail.com>
2022-08-10 01:59:55 +0000

    limit extract results to 10

--- apps/ite-portal/src/app/submissions-list/submissions-list.component.ts
+++ apps/ite-portal/src/app/submissions-list/submissions-list.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { ExtractSubmission } from '../ingest-extract';
 
 @Component({
@@ -9,9 +9,9 @@ import { ExtractSubmission } from '../ingest-extract';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubmissionsListComponent {
-  submissions$: Observable<ExtractSubmission[]> = this.http.get<
-    ExtractSubmission[]
-  >('https://ite-api.herokuapp.com/api/v1/extracts');
+  submissions$: Observable<ExtractSubmission[]> = this.http
+    .get<ExtractSubmission[]>('https://ite-api.herokuapp.com/api/v1/extracts')
+    .pipe(map((extracts) => extracts.slice(0, 10)));
 
   constructor(private http: HttpClient) {}
 }
