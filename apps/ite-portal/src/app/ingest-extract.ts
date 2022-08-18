e04e97d6197ace0f2f1a1834af8e3e91e0f990e5
Kristin Merbach <kristin@kristinmerbach.com>
2022-08-18 10:52:30 -0700

    change previous submission page to include pass/fail as opposed to number of warnings/failures

--- apps/ite-portal/src/app/ingest-extract.ts
+++ apps/ite-portal/src/app/ingest-extract.ts
@@ -5,7 +5,7 @@ export interface ExtractSubmission {
   submission_date: string;
   file_type: 'Initial' | null;
   number_of_records: number | null;
-  record_failure_count: number | null;
-  record_warning_count: number | null;
+  fail_count: number | null;
+  pass_count: number | null;
   status: 'Valid' | 'Invalid';
 }
