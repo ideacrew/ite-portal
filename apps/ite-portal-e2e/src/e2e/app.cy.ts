64582589e47a07197d723206fc2ec13c48b722f8
Mark Goho <markgoho@gmail.com>
2022-08-29 18:01:59 -0400

    add failing test for csv with comma-first row

--- apps/ite-portal-e2e/src/e2e/app.cy.ts
+++ apps/ite-portal-e2e/src/e2e/app.cy.ts
@@ -74,6 +74,24 @@ describe('ite-portal', () => {
     });
   });
 
+  it('should fill out the entire form with even more valid data', () => {
+    // Fill out the form
+    const today = new Date().toISOString().slice(0, 10);
+    getExtractDate().type(today);
+    cy.get('[data-cy="file-upload"]').selectFile('src/fixtures/test4.csv');
+    cy.get('[data-cy="submit-extract"]').should('not.be.disabled');
+    cy.get('[data-cy="submit-extract"]').click();
+
+    // Wait for API call to complete
+    cy.wait('@submitExtract').then(({ request }) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+      const records: unknown[] = request.body.records;
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(records.length).to.equal(15);
+    });
+  });
+
   it('should show error message on invalid csv', () => {
     const dateInThePast = new Date('2020-01-01').toISOString().slice(0, 10);
     getExtractDate().type(dateInThePast);
