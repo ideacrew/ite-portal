12820ed3c885378493faffe8ba17580f12791c07
Mark Goho <markgoho@gmail.com>
2022-08-30 17:58:32 +0000

    sort records only by fatal errors for now

--- apps/ite-portal/src/app/record-sort.pipe.ts
+++ apps/ite-portal/src/app/record-sort.pipe.ts
@@ -1,4 +1,6 @@
+/* eslint-disable unicorn/no-nested-ternary */
 import { Pipe, PipeTransform } from '@angular/core';
+
 import { ExtractRecordValidationV2 } from '@dbh/provider-extract/data-access';
 
 @Pipe({
@@ -8,33 +10,16 @@ export class RecordSortPipe implements PipeTransform {
   transform(records: ExtractRecordValidationV2[]): ExtractRecordValidationV2[] {
     const unsortedRecords = [...records];
 
-    const sortedRecords = unsortedRecords
-      .sort((recordA, recordB) => {
-        const countOfRecordAFatalErrors = recordA.errors.filter(
-          (error) => error.errorType === 'Fatal'
-        ).length;
-        const countOfRecordBFatalErrors = recordB.errors.filter(
-          (error) => error.errorType === 'Fatal'
-        ).length;
+    const sortedRecords = unsortedRecords.sort((recordA, recordB) => {
+      const countOfRecordAFatalErrors = recordA.errors.filter(
+        (error) => error.errorType === 'Fatal'
+      ).length;
+      const countOfRecordBFatalErrors = recordB.errors.filter(
+        (error) => error.errorType === 'Fatal'
+      ).length;
 
-        const fatalComparison =
-          countOfRecordAFatalErrors > countOfRecordBFatalErrors;
-
-        return fatalComparison ? -1 : 1;
-      })
-      .sort((recordA, recordB) => {
-        const countOfRecordACriticalErrors = recordA.errors.filter(
-          (error) => error.errorType == 'Critical'
-        ).length;
-        const countOfRecordBCriticalErrors = recordB.errors.filter(
-          (error) => error.errorType == 'Critical'
-        ).length;
-
-        const criticalComparison =
-          countOfRecordACriticalErrors > countOfRecordBCriticalErrors;
-
-        return criticalComparison ? -1 : 1;
-      });
+      return countOfRecordAFatalErrors > countOfRecordBFatalErrors ? -1 : 1;
+    });
 
     return sortedRecords;
   }
