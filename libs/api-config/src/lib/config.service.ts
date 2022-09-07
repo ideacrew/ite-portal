e6f658d01b397b275acd1d7780b7de0bb27b121d
Mark Goho <markgoho@gmail.com>
2022-09-07 10:53:59 -0400

    update config service to use proper env api

--- libs/api-config/src/lib/config.service.ts
+++ libs/api-config/src/lib/config.service.ts
@@ -9,14 +9,13 @@ export class ConfigService {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {
     const [urlEnvironment] = window.location.host.split('.');
-
     if (
-      urlEnvironment.includes('dbh-ite') &&
-      !urlEnvironment.includes('staging')
+      urlEnvironment.includes('portal-dev') &&
+      !urlEnvironment.includes('portal-uat')
     ) {
       this.baseApiUrl = `https://ite-api.herokuapp.com`;
       this.environment = 'dev';
-    } else if (urlEnvironment.includes('staging')) {
+    } else if (urlEnvironment.includes('portal-uat')) {
       this.baseApiUrl = `https://ite-api-staging.herokuapp.com`;
       this.environment = 'uat';
     } else {
