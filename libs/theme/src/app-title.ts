89a5d223ca533644b4ce5099f016ba11139b5961
Mark Goho <markgoho@gmail.com>
2022-09-22 14:24:03 +0000

    trigger deployment

--- libs/theme/src/app-title.ts
+++ libs/theme/src/app-title.ts
@@ -1,3 +1,5 @@
 import { InjectionToken } from '@angular/core';
 
 export const APP_TITLE = new InjectionToken<string>('appTitle');
+
+export const SOMETHING_ELSE = new InjectionToken<string>('somethingElse');
