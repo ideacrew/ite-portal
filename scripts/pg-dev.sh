35adfd099f5d87a8652d0131b0d47512868eccf7
Mark Goho <markgoho@gmail.com>
2022-09-22 13:57:54 +0000

    fix s3 sync command

--- scripts/pg-dev.sh
+++ scripts/pg-dev.sh
@@ -1,4 +1,4 @@
 cd dist/apps/provider-gateway
-aws s3 sync . s3://provider-gateway-dev-dbh-ite-com/ --delete
+aws s3 sync --acl public-read . s3://provider-gateway-dev-dbh-ite-com/ --delete
 invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E37CU4MXO6RHYU --output text --query 'Invalidation.Id' --paths '/*'`
 aws cloudfront wait invalidation-completed --distribution-id E37CU4MXO6RHYU --id ${invalidationId}
