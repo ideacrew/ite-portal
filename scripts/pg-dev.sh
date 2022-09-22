b9637c6306ff9a69f65df82c12574227797f9eab
Mark Goho <markgoho@gmail.com>
2022-09-22 13:39:41 +0000

    try running deploy from script

---
+++ scripts/pg-dev.sh
@@ -0,0 +1,4 @@
+cd dist/apps/provider-gateway
+aws s3 sync . s3://provider-gateway-dev-dbh-ite-com/ --delete
+invalidationId=`AWS_MAX_ATTEMPTS=999 aws cloudfront create-invalidation --distribution-id E37CU4MXO6RHYU --output text --query 'Invalidation.Id' --paths '/*'`
+aws cloudfront wait invalidation-completed --distribution-id E37CU4MXO6RHYU --id ${invalidationId}
