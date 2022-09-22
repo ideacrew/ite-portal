27716c557ba9ccf0fd2a955ef4ce22ab62b3a018
Mark Goho <markgoho@gmail.com>
2022-09-22 14:55:31 +0000

    actually run scripts

--- libs/theme/src/app-title.ts
+++ libs/theme/src/app-title.ts
@@ -2,4 +2,4 @@ import { InjectionToken } from '@angular/core';
 
 export const APP_TITLE = new InjectionToken<string>('appTitle');
 
-// export const SOMETHING_ELSE = new InjectionToken<string>('somethingElse');
+export const SOMETHING_ELSE = new InjectionToken<string>('somethingElse');
