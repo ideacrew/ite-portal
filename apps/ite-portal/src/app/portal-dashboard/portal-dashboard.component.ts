92e441f033e2094d84afd939cab7ec4a1840194b
Mark Goho <markgoho@gmail.com>
2022-09-27 23:09:48 +0000

    add provider gateway widget

--- apps/ite-portal/src/app/portal-dashboard/portal-dashboard.component.ts
+++ apps/ite-portal/src/app/portal-dashboard/portal-dashboard.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { BHSDService } from '@dbh/bhsd/data-access';
 
 @Component({
   templateUrl: './portal-dashboard.component.html',
   styleUrls: ['./portal-dashboard.component.scss'],
 })
-export class PortalDashboardComponent {}
+export class PortalDashboardComponent {
+  submissionStatus$ = this.bhsdService.getSubmissionStatus();
+  constructor(private bhsdService: BHSDService) {}
+}
