8ee8d05b81c3af4ddf288341b0823762c3144ddd
Mark Goho <markgoho@gmail.com>
2022-09-27 23:47:27 +0000

    add additional widgets

--- apps/ite-portal/src/app/portal-dashboard/portal-dashboard.component.ts
+++ apps/ite-portal/src/app/portal-dashboard/portal-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { BHSDService } from '@dbh/bhsd/data-access';
 
 @Component({
@@ -7,5 +8,9 @@ import { BHSDService } from '@dbh/bhsd/data-access';
 })
 export class PortalDashboardComponent {
   submissionStatus$ = this.bhsdService.getSubmissionStatus();
-  constructor(private bhsdService: BHSDService) {}
+  constructor(private bhsdService: BHSDService, private router: Router) {}
+
+  navigateTo(path: string): void {
+    void this.router.navigate([path]);
+  }
 }
