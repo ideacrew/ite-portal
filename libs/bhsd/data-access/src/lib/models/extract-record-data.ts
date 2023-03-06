a29d967aeb3780e40c368f63399c1727d029f776
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2023-03-06 13:41:54 -0800

    census breakdown (#30)
    * census_breakdown
    
    * add additional view toggle
    
    * add conditionals to fields
    
    * fix spec

--- libs/bhsd/data-access/src/lib/models/extract-record-data.ts
+++ libs/bhsd/data-access/src/lib/models/extract-record-data.ts
@@ -21,6 +21,8 @@ export interface ExtractRecordData {
   race: string;
   record_type: string;
   treatment_setting: string;
+  co_occurring_sud_mh?: string | undefined;
+  discharge_date?: string | undefined;
 }
 
 export type ExtractRecordField = keyof ExtractRecordData;
---
+++ libs/bhsd/data-access/src/lib/util/get-census-data.ts
@@ -0,0 +1,146 @@
+import {
+  Metric,
+  ExtractRecordValidation,
+  ExtractSubmissionResponse,
+  ExtractSubmissionCensusBreakdown,
+} from '../models';
+
+export const convertExtractToCensus = (
+  submission: ExtractSubmissionResponse,
+  reportingPeriod: Date
+): ExtractSubmissionCensusBreakdown => {
+  const { records } = submission;
+  const lastDayOfMonth = new Date(
+    reportingPeriod.getFullYear(),
+    reportingPeriod.getMonth() + 1,
+    0
+  );
+  const totalEpisodes = getMetrics(records);
+  const censusAtStartOfReportingPeriod = getMetrics(
+    records.filter(
+      (record) =>
+        getDate(record.payload.admission_date) !== 'Invalid' &&
+        new Date(record.payload.admission_date).getTime() <
+          reportingPeriod.getTime()
+    )
+  );
+  const admissionsTransfersDuringReportingPeriod = getMetrics(
+    records.filter(
+      (record) =>
+        getDate(record.payload.admission_date) !== 'Invalid' &&
+        new Date(record.payload.admission_date).getTime() >=
+          reportingPeriod.getTime() &&
+        new Date(record.payload.admission_date).getTime() <=
+          lastDayOfMonth.getTime()
+    )
+  );
+  const countOfUniqueClientsServedDuringReportingPeriod =
+    getUniqueClientMetrics(records);
+  const dischargesDuringReportingPeriod = getMetrics(
+    records.filter(
+      (record) =>
+        record.payload.discharge_date &&
+        getDate(record.payload.discharge_date) !== 'Invalid' &&
+        new Date(record.payload.discharge_date).getTime() >=
+          reportingPeriod.getTime() &&
+        new Date(record.payload.discharge_date).getTime() <=
+          lastDayOfMonth.getTime()
+    )
+  );
+  const censusAtEndOfReportingPeriod = getMetrics(
+    records.filter(
+      (record) =>
+        record.payload.discharge_date === undefined ||
+        record.payload.discharge_date === null ||
+        record.payload.discharge_date === ''
+    )
+  );
+  return {
+    totalEpisodes,
+    censusAtStartOfReportingPeriod,
+    admissionsTransfersDuringReportingPeriod,
+    dischargesDuringReportingPeriod,
+    censusAtEndOfReportingPeriod,
+    countOfUniqueClientsServedDuringReportingPeriod,
+  };
+};
+
+export const getMetrics = (records: ExtractRecordValidation[]): Metric => {
+  const total = records.length;
+  const mhInitialAdmissions = records.filter(
+    (record) => record.payload.record_type === 'M'
+  ).length;
+  const mhTransfers = records.filter(
+    (record) => record.payload.record_type === 'X'
+  ).length;
+  const sudInitialAdmissions = records.filter(
+    (record) => record.payload.record_type === 'A'
+  ).length;
+  const sudTransfers = records.filter(
+    (record) => record.payload.record_type === 'T'
+  ).length;
+  const coOccurringRecords = records.filter(
+    (record) => record.payload.co_occurring_sud_mh
+  ).length;
+  return {
+    total,
+    mhInitialAdmissions,
+    mhTransfers,
+    sudInitialAdmissions,
+    sudTransfers,
+    coOccurringRecords,
+  };
+};
+
+export const getUniqueClientMetrics = (
+  records: ExtractRecordValidation[]
+): Metric => {
+  const total = getGroup(records).size;
+  const mhInitialAdmissions = getGroup(
+    records.filter((record) => record.payload.record_type === 'M')
+  ).size;
+  const mhTransfers = getGroup(
+    records.filter((record) => record.payload.record_type === 'X')
+  ).size;
+  const sudInitialAdmissions = getGroup(
+    records.filter((record) => record.payload.record_type === 'A')
+  ).size;
+  const sudTransfers = getGroup(
+    records.filter((record) => record.payload.record_type === 'T')
+  ).size;
+  const coOccurringRecords = getGroup(
+    records.filter((record) => record.payload.co_occurring_sud_mh)
+  ).size;
+  return {
+    total,
+    mhInitialAdmissions,
+    mhTransfers,
+    sudInitialAdmissions,
+    sudTransfers,
+    coOccurringRecords,
+  };
+};
+
+const getDate = (dateString: string): Date | 'Invalid' => {
+  const dateRegex1 = /^(0?[1-9]|1[0-2])\/(0?[1-9]|1\d|2\d|3[01])\/\d{4}$/;
+  const dateRegex2 = /^\d{4}-\d{2}-\d{2}$/;
+
+  return dateRegex1.test(dateString) || dateRegex2.test(dateString)
+    ? new Date(dateString)
+    : 'Invalid';
+};
+
+const getGroup = (
+  records: ExtractRecordValidation[]
+): Map<string, ExtractRecordValidation[]> => {
+  const groupedMap = new Map<string, ExtractRecordValidation[]>();
+  for (const record of records) {
+    const thisArray = groupedMap.get(record.payload.client_id);
+    if (thisArray !== undefined) {
+      thisArray.push(record);
+    } else {
+      groupedMap.set(record.payload.client_id, [record]);
+    }
+  }
+  return groupedMap;
+};
