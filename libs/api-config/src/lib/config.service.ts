3e80636b3199ce16becf88afa5060b615fa58d2a
Kristin Merbach <kristin@kristinmerbach.com>
2023-05-09 09:21:57 -0700

    styling

--- libs/api-config/src/lib/config.service.ts
+++ libs/api-config/src/lib/config.service.ts
@@ -5,10 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
   // By default the gatewayApiUrl is set to the dev api url
-  //gatewayApiUrl = 'http://localhost:4002';
-  gatewayApiUrl = 'https://bff-dev.dbh-ite.com';
-  //portalApiUrl = 'http://localhost:4000';
-  portalApiUrl = 'https://portal-bff-dev.dbh-ite.com/';
+  gatewayApiUrl = 'http://localhost:4002';
+  //gatewayApiUrl = 'https://bff-dev.dbh-ite.com';
+  portalApiUrl = 'http://localhost:4000';
+  //portalApiUrl = 'https://portal-bff-dev.dbh-ite.com/';
 
   constructor() {
     // Only if the url contains uat or only the application name should we override the api url
