95cc6151cef7be3d3ffa1f31664125fd41425fe3
Kristin Merbach <kristin@kristinmerbach.com>
2023-05-18 12:18:15 -0700

    check if a dbh user is also a provider

--- apps/ite-portal/src/app/portal/portal.component.ts
+++ apps/ite-portal/src/app/portal/portal.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
 import { EventMessage, EventType } from '@azure/msal-browser';
+import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
+import { ConfigService } from '@dbh/api-config';
+
+type ProfileStatus = {
+  providerStatus: boolean;
+  dbhStatus: boolean;
+  pgId?: string;
+  pId?: string;
+};
 @Component({
   templateUrl: './portal.component.html',
   styleUrls: ['./portal.component.scss'],
 })
 export class PortalComponent implements OnInit {
-  isProvider = true;
+  isProvider = false;
   isDBHUser = true;
 
   applicationName = 'ITE Portal';
@@ -17,12 +26,15 @@ export class PortalComponent implements OnInit {
 
   constructor(
     private authService: MsalService,
-    private msalBroadcastService: MsalBroadcastService
+    private msalBroadcastService: MsalBroadcastService,
+    private config: ConfigService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
     if (this.authService.instance.getAllAccounts().length > 0) {
       this.email = this.authService.instance.getAllAccounts()[0]['username'];
+      this.setProviderDetails();
     }
     this.msalBroadcastService.msalSubject$
       .pipe(
@@ -33,6 +45,18 @@ export class PortalComponent implements OnInit {
       )
       .subscribe((result: EventMessage) => {
         console.log(result);
+      });
+  }
+
+  // set provider details from the provider profile
+  setProviderDetails(): void {
+    this.http
+      .get<ProfileStatus>(
+        `${this.config.gatewayApiUrl}/api/v1/providers/provider_status`
+      )
+      .subscribe((result) => {
+        this.isProvider = result.providerStatus;
+        this.isDBHUser = result.dbhStatus;
       });
   }
 }
