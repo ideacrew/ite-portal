5d6fb81f9619697028a0015b8e47a6468dea017b
Kristin Merbach <kristin@kristinmerbach.com>
2023-08-24 10:29:19 -0700

    configure azure url pattern

--- libs/api-config/src/lib/config.service.ts
+++ libs/api-config/src/lib/config.service.ts
@@ -4,22 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ConfigService {
-  // By default the gatewayApiUrl is set to the dev api url
-  // gatewayApiUrl = 'http://localhost:4002';
-  gatewayApiUrl = 'https://bff-dev.dbh-ite.com';
-  //portalApiUrl = 'http://localhost:4000';
-  portalApiUrl = 'https://portal-bff-dev.dbh-ite.com/';
+  // By default the gatewayApiUrl is set to azure pattern of urls
+  gatewayApiUrl = 'https://api.' + window.location.hostname;
+  portalApiUrl = 'https://api.' + window.location.hostname;
 
   constructor() {
     // Only if the url contains uat or only the application name should we override the api url
     const [urlEnvironment] = window.location.host.split('.');
-
-    if (urlEnvironment.includes('uat')) {
-      this.gatewayApiUrl = `https://bff-uat.dbh-ite.com`;
-    }
-
-    if (urlEnvironment === 'portal' || urlEnvironment === 'provider-gateway') {
-      this.gatewayApiUrl = `https://bff.dbh-ite.com`;
+    // for local dev
+    if (window.location.host.includes('localhost')) {
+      this.gatewayApiUrl = 'http://localhost:4001';
+      this.portalApiUrl = 'http://localhost:4000';
+    } else {
+      // for old aws urls. this will be removed once fully migrated to azure
+      this.gatewayApiUrl = 'https://bff-dev.dbh-ite.com';
+      this.portalApiUrl = 'https://portal-bff-dev.dbh-ite.com/';
+      if (urlEnvironment.includes('uat')) {
+        this.gatewayApiUrl = `https://bff-uat.dbh-ite.com`;
+      }
+      if (
+        urlEnvironment === 'portal' ||
+        urlEnvironment === 'provider-gateway'
+      ) {
+        this.gatewayApiUrl = `https://bff.dbh-ite.com`;
+      }
     }
   }
 }
