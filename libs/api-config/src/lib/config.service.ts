eba01cc8b71bb7dd4cf32479f8ffba4186fe44d0
Kristin Merbach <kristin@kristinmerbach.com>
2023-09-25 12:46:51 -0700

    check for aws urls exactly

--- libs/api-config/src/lib/config.service.ts
+++ libs/api-config/src/lib/config.service.ts
@@ -18,19 +18,19 @@ export class ConfigService {
     } else if (window.location.host.includes('localhost')) {
       this.gatewayApiUrl = 'http://localhost:4001';
       this.portalApiUrl = 'http://localhost:4000';
-    } else if (!window.location.hostname.includes('dbhite')) {
+    } else if (window.location.hostname.includes('dbh-ite')) {
       // for old aws deployments. this will be removed once fully migrated to azure.
       this.gatewayApiUrl = 'https://bff-dev.dbh-ite.com';
       this.portalApiUrl = 'https://portal-bff-dev.dbh-ite.com/';
       if (urlEnvironment.includes('uat')) {
         this.gatewayApiUrl = `https://bff-uat.dbh-ite.com`;
       }
-      // if (
-      //   urlEnvironment === 'portal' ||
-      //   urlEnvironment === 'provider-gateway'
-      // ) {
-      //   this.gatewayApiUrl = `https://bff.dbh-ite.com`;
-      // }
+      if (
+        urlEnvironment === 'portal' ||
+        urlEnvironment === 'provider-gateway'
+      ) {
+        this.gatewayApiUrl = `https://bff.dbh-ite.com`;
+      }
     }
   }
 
