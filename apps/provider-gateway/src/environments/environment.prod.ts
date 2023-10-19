e38f3d4b31e15f42ab6c00738a2ab8475c2b71e7
Kristin Merbach <kristin@kristinmerbach.com>
2023-10-19 07:42:20 -0700

    add envs to prod

--- apps/provider-gateway/src/environments/environment.prod.ts
+++ apps/provider-gateway/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export const environment = {
   production: true,
-  gatewayApi: 'https://api-provider.dbh.dc.gov',
-  portalApi: 'https://api-portal.dbh.dc.gov',
+  NX_GATEWAY_API: 'https://api-provider.dbh.dc.gov',
+  NX_PORTAL_API: 'https://api-portal.dbh.dc.gov',
   B2C_SUBDOMAIN: process.env['NX_B2C_SUBDOMAIN_PROD'],
   NX_GATEWAY_C_ID: process.env['NX_GATEWAY_C_ID_PROD'],
 };
