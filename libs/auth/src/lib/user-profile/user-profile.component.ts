87509bd3f48721497bbf7d1da609ea39d687d6b9
Kristin Merbach <kristin@kristinmerbach.com>
2023-10-18 20:34:53 -0700

    b2c for provider auth

--- libs/auth/src/lib/user-profile/user-profile.component.ts
+++ libs/auth/src/lib/user-profile/user-profile.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { Component, OnInit } from '@angular/core';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
 
 @Component({
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent {
-  email = this.authService.email;
+  email = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService
+  ) {}
+
+  ngOnInit(): void {
+    const account = this.authService.instance.getActiveAccount();
+    console.log(account);
+    console.log(this.authService.instance.getActiveAccount());
+    if (this.authService.instance.getAllAccounts().length > 0) {
+      console.log('got accounts');
+      this.email = this.authService.instance.getAllAccounts()[0]['username'];
+    }
+  }
 
   logout(): void {
-    this.authService.logout();
+    this.authService.logoutRedirect({
+      postLogoutRedirectUri: window.location.origin,
+    });
   }
 }
