2ee0d2835493554df654debc2c3046dcd506cd27
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2023-10-23 07:27:47 -0700

    b2c for provider auth (#89)
    * b2c for provider auth
    
    * remove test GHA
    
    * skip tests for pg now too
    
    * remove console.logs
    
    * fix missing bracket
    
    * remove bad redirects, clean up code
    
    * add envs to prod
    
    * cleanup based on feedback

--- libs/auth/src/lib/user-profile/user-profile.component.ts
+++ libs/auth/src/lib/user-profile/user-profile.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { Component, OnInit } from '@angular/core';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
 
 @Component({
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {
-  email = this.authService.email;
+export class UserProfileComponent implements OnInit {
+  email = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.authService.instance.getAllAccounts().length > 0) {
+      this.email = this.authService.instance.getAllAccounts()[0]['username'];
+    }
+  }
 
   logout(): void {
-    this.authService.logout();
+    this.authService.logoutRedirect({
+      postLogoutRedirectUri: window.location.origin,
+    });
   }
 }
