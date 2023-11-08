671ffa5dc6bea2a702550b56030f407ca726fcc6
Brad Bodine <bbodine1@gmail.com>
2023-11-07 18:08:23 -0600

    add UTC message and sort by provider

--- apps/ite-portal/src/app/executive/provider-logins.component.ts
+++ apps/ite-portal/src/app/executive/provider-logins.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BHSDService } from '@dbh/bhsd/data-access';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ProvidersLog } from '@dbh/providers/data-access';
 
 @Component({
@@ -11,7 +11,12 @@ import { ProvidersLog } from '@dbh/providers/data-access';
   template: `
     <h1>Provider Recent Logins</h1>
 
-    <table *ngIf="testing$ | async as results">
+    <small>
+      <span class="material-symbols-outlined"> info </span>
+      All times are displayed in UTC
+    </small>
+
+    <table *ngIf="sortedProviders$ | async as results">
       <thead>
         <th>Provider Name</th>
         <th>Last Login</th>
@@ -22,7 +27,7 @@ import { ProvidersLog } from '@dbh/providers/data-access';
         <tr *ngFor="let user of results.users">
           <td>{{ user.provider_name }}</td>
           <td *ngIf="user.last_sign_in_at; else never">
-            {{ user.last_sign_in_at | date : 'medium' }}
+            {{ user.last_sign_in_at | date : 'long' : 'UTC' }}
           </td>
           <ng-template #never>
             <td>Never</td>
@@ -33,11 +38,40 @@ import { ProvidersLog } from '@dbh/providers/data-access';
       </tbody>
     </table>
   `,
-  styles: [],
+  styles: [
+    `
+      small {
+        display: inline-flex;
+        align-items: center;
+        gap: 4px;
+        font-size: 12px;
+        margin-bottom: 16px;
+        background-color: var(--primary-020);
+        color: var(--text-primary);
+        border-radius: 4px;
+        padding: 1px 4px;
+      }
+
+      span[class^='material'] {
+        width: 16px;
+        height: 16px;
+        font-size: 16px;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProviderLoginsComponent {
-  testing$: Observable<ProvidersLog> = this.bhsdService.getProviderLogins();
+  providers$: Observable<ProvidersLog> = this.bhsdService.getProviderLogins();
+  // Sort by the provider name
+  sortedProviders$ = this.providers$.pipe(
+    map((results) => ({
+      ...results,
+      users: results.users.sort((a, b) =>
+        a.provider_name.localeCompare(b.provider_name)
+      ),
+    }))
+  );
 
   constructor(private bhsdService: BHSDService) {}
 }
