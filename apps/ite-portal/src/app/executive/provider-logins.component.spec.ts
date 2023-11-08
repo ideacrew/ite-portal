b4e9c8c6f2b5fe20eedbc2148945d7e3e8e55df5
Brad Bodine <bbodine1@gmail.com>
2023-11-08 09:49:17 -0600

    fix broken test

--- apps/ite-portal/src/app/executive/provider-logins.component.spec.ts
+++ apps/ite-portal/src/app/executive/provider-logins.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { ProviderLoginsComponent } from './provider-logins.component';
 
 describe('ProviderLoginsComponent', () => {
   let component: ProviderLoginsComponent;
   let fixture: ComponentFixture<ProviderLoginsComponent>;
 
+  const activatedRouteMock = {
+    paramMap: of({ get: () => '1' }),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProviderLoginsComponent, HttpClientTestingModule],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRouteMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProviderLoginsComponent);
