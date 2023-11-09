09839076d8d88d641d27a9246f0c451c47ef6dd3
Kristin Merbach <kristin@kristinmerbach.com>
2023-11-09 10:13:41 -0800

    non visible change rather than merge commit

--- apps/ite-portal/src/app/executive/provider-logins.component.ts
+++ apps/ite-portal/src/app/executive/provider-logins.component.ts
@@ -21,7 +21,7 @@ import { RouterLink } from '@angular/router';
 
     <h1>Provider Logins</h1>
 
-    <small class="non-visible-change">
+    <small class="nonvisiblechange">
       <span class="material-symbols-outlined"> info </span>
       All times are displayed in UTC
     </small>
