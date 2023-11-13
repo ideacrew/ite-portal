0308b43adf4b02fe1cae466bb054d129a6485926
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2023-11-13 07:14:53 -0800

    adjust pass rate to round to one decimal (#118)
    * only round things to one decimal place
    
    * undoing overkill changes
    
    * fix environments files
    
    * fix index html

--- libs/bhsd/util/src/lib/pass-rate.ts
+++ libs/bhsd/util/src/lib/pass-rate.ts
@@ -2,7 +2,7 @@ import { SubmissionStatus } from '@dbh/bhsd/data-access';
 
 export const passRate = ({ pass, totalRecords }: Partial<SubmissionStatus>) => {
   if (pass && totalRecords) {
-    return `${Math.round((pass / totalRecords) * 100)}%`;
+    return `${Math.round((pass / totalRecords) * 1000) / 10}%`;
   } else if (totalRecords && !pass) {
     return '0%';
   } else {
