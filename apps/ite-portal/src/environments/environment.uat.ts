1b9081f735c1ff476984e77f0b78bf8928b986ff
Kristin Merbach <kristin@kristinmerbach.com>
2023-11-16 07:54:01 -0800

    started adding the new env

---
+++ apps/ite-portal/src/environments/environment.uat.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+export const environment = {
+  production: true,
+  gatewayApi: 'https://api-provider.dbh.dc.gov',
+  portalApi: 'https://api-portal.dbh.dc.gov',
+  NX_AD_CLIENT_ID: process.env['NX_AD_CLIENT_ID_UAT'],
+  NX_AD_TID_PROD: process.env['NX_AD_TID_UAT'],
+};
