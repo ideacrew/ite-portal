b5a02ad94ae350496873b83b056b14e58bfbc7e9
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2023-11-16 08:55:54 -0800

    started adding the new env (#125)
    * started adding the new env
    
    * add environment variables
    
    * add urls to config file

--- apps/ite-portal/src/environments/environment.prod.ts
+++ apps/ite-portal/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@
 
 export const environment = {
   production: true,
-  gatewayApi: 'https://api-provider.dbh.dc.gov',
-  portalApi: 'https://api-portal.dbh.dc.gov',
+  gatewayApi: 'https://provider-gateway-api.dbh.dc.gov',
+  portalApi: 'https://ite-api.dbh.dc.gov',
   NX_AD_CLIENT_ID: process.env['NX_AD_CLIENT_ID_PROD'],
   NX_AD_TID_PROD: process.env['NX_AD_TID_PROD'],
 };
