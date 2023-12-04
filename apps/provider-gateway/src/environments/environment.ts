6d4fae3b8e2915bbe76946ba9ca78df7f45d6fcb
Brad Bodine <bbodine1@gmail.com>
2023-12-04 17:23:35 -0600

    update app timeout to 15 min

--- apps/provider-gateway/src/environments/environment.ts
+++ apps/provider-gateway/src/environments/environment.ts
@@ -8,8 +8,8 @@ const gatewayApiUrl = 'https://api.provider.dev.dbhite.com';
 const portalApiUrl = 'https://api.portal.dev.dbhite.com';
 const authority = `https://${subdomain}.b2clogin.com/${subdomain}.onmicrosoft.com/b2c_1_sign_in_1`;
 const authorityDomain = `${subdomain}.b2clogin.com`;
-// const appInactiveTimeout = 900; // 15 minutes
-const appInactiveTimeout = 10; // 10 seconds
+const appInactiveTimeout = 900; // 15 minutes
+// const appInactiveTimeout = 10; // 10 seconds
 
 export const environment = {
   production: false,
--- apps/provider-gateway/src/environments/environment.uat.ts
+++ apps/provider-gateway/src/environments/environment.uat.ts
@@ -4,8 +4,8 @@ const gatewayApiUrl = 'https://api-provider.dbh.dc.gov';
 const portalApiUrl = 'https://api-portal.dbh.dc.gov';
 const authority = `https://${subdomain}.b2clogin.com/${subdomain}.onmicrosoft.com/b2c_1_sign_in_1`;
 const authorityDomain = `${subdomain}.b2clogin.com`;
-// const appInactiveTimeout = 900; // 15 minutes
-const appInactiveTimeout = 10; // 10 seconds
+const appInactiveTimeout = 900; // 15 minutes
+// const appInactiveTimeout = 10; // 10 seconds
 
 export const environment = {
   production: false,
