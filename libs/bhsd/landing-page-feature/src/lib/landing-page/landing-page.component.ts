026f594fc747996902f919e60be268bd90f2d27f
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2023-12-15 05:35:34 -0800

    remove inactive providers from charts (#149)

--- libs/bhsd/landing-page-feature/src/lib/landing-page/landing-page.component.ts
+++ libs/bhsd/landing-page-feature/src/lib/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BHSDService } from '@dbh/bhsd/data-access';
 import { getReportingPeriod, getReportingPeriodText } from '@dbh/bhsd/util';
+import { SubmissionStatus } from '@dbh/bhsd/data-access';
 
 @Component({
   templateUrl: './landing-page.component.html',
@@ -11,7 +12,7 @@ export class LandingPageComponent {
   today = new Date();
   lastMonth = getReportingPeriod(1);
   prevSubmissionMonth = getReportingPeriod(2);
-
+  activeSubmissions: SubmissionStatus[] = [];
   submissionStatus$ = this.bhsdService.getSubmissionStatusByDate({
     month: this.lastMonth.getMonth(),
     year: this.lastMonth.getFullYear(),
@@ -21,6 +22,13 @@ export class LandingPageComponent {
     month: this.prevSubmissionMonth.getMonth(),
     year: this.lastMonth.getFullYear(),
   });
+
+  activeSubmissions$ = this.submissionStatus$.subscribe(
+    (response: SubmissionStatus[]) => {
+      const submissions = response.filter(submission => submission.providerActiveStatus);
+      this.activeSubmissions = submissions;
+    }
+  );
 
   thisReportingPeriod = getReportingPeriodText(this.lastMonth);
   lastReportingPeriod = getReportingPeriodText(this.prevSubmissionMonth);
